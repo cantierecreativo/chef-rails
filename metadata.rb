1cebd4f99e6e5ac9142dca8e8573afa2deba79cb
David Librera <davidlibrera@gmail.com>
2016-06-28 14:20:08 +0200

    dump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'd.librera@cantierecreativo.net'
 license          'All rights reserved'
 description      'Installs/Configures rails'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 depends "swap"
 depends "certbot"
