8f45fcf02330d38eeec18b50ea4f3fa95148c4e0
David Librera <davidlibrera@gmail.com>
2016-06-28 14:56:16 +0200

    fixed nginx cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'd.librera@cantierecreativo.net'
 license          'All rights reserved'
 description      'Installs/Configures rails'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.3'
+version          '0.2.4'
 
 depends "swap"
 depends "certbot"
-depends "nginx"
+depends "nginx",    ">= 2.7.6"
 depends "postgresql"
 depends "capistrano-rails"
 depends "ruby_rbenv"
