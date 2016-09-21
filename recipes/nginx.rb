55e830c80f2b85eb47a3c44f3e66b0a3509f5a8b
Joe Yates <joe.g.yates@gmail.com>
2016-09-21 15:51:35 +0200

    Delete any Nginx-created symlink

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -4,7 +4,7 @@ nginx_enabled_path = "/etc/nginx/sites-enabled"
 nginx_default_conf = File.join(nginx_enabled_path, "default")
 nginx_default_404_conf = File.join(nginx_enabled_path, "default_404")
 
-file nginx_default_conf do
+link nginx_default_conf do
   action :delete
 end
 
