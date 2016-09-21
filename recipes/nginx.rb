4aa8005962f25171ce0bd86e2d679ef228142a34
Joe Yates <joe.g.yates@gmail.com>
2016-09-21 11:12:14 +0200

    Use a different name for out Nginx config
    * avoid problems when replacing the Nginx-installed symlink
      with our config file.

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -1,9 +1,17 @@
 package "nginx-core"
 
-cookbook_file '/etc/nginx/sites-enabled/default' do
-  source 'nginx_default.conf'
+nginx_enabled_path = "/etc/nginx/sites-enabled"
+nginx_default_conf = File.join(nginx_enabled_path, "default")
+nginx_default_404_conf = File.join(nginx_enabled_path, "default_404")
+
+file nginx_default_conf do
+  action :delete
+end
+
+cookbook_file nginx_default_404_conf do
+  source "nginx_default.conf"
   owner "root"
   group "root"
-  mode '0644'
+  mode "0644"
   action :create
 end
